c6ea3e618baa4cebe817dfd4c1fb40ee13e2a111
sm8sk4l <sebastianmoskal@protonmail.com>
2023-06-07 23:54:22 +0200

    project service && createProjectForm UI

---
+++ Projekt/src/app/services/project.service.ts
@@ -0,0 +1,69 @@
+import { Injectable } from '@angular/core';
+import { ProjectInterface } from '../interfaces/project.interface';
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProjectService {
+  private localStorageKey = 'tasks';
+  private projects: ProjectInterface[] = [];
+
+  private getDataFromLocalStorage(): void {
+    const data = localStorage.getItem(this.localStorageKey);
+    if (data) this.projects = JSON.parse(data);
+  }
+
+  private saveDataToLocalStorage(): void {
+    localStorage.setItem(this.localStorageKey, JSON.stringify(this.projects));
+  }
+
+  constructor() {
+    this.getDataFromLocalStorage();
+  }
+
+  getProjects(): Observable<ProjectInterface[]> {
+    return of(this.projects);
+  }
+
+  getSingleProject(ID: string): Observable<ProjectInterface> {
+    const project = this.projects.find((p) => p.ID === ID);
+
+    if (project) {
+      return of(project);
+    } else {
+      throw new Error('Task not found');
+    }
+  }
+
+  createProject(project: ProjectInterface): Observable<ProjectInterface> {
+    this.projects.push(project);
+    this.saveDataToLocalStorage();
+    return of(project);
+  }
+
+  updateProject(project: ProjectInterface): Observable<ProjectInterface> {
+    const projectToUpdate = this.projects.find((p) => p.ID === project.name);
+
+    if (projectToUpdate) {
+      projectToUpdate.name = project.name;
+      projectToUpdate.description = project.description;
+      this.saveDataToLocalStorage();
+      return of(projectToUpdate);
+    } else {
+      return of();
+    }
+  }
+
+  deleteProject(ID: string): Observable<boolean> {
+    const newProjectsList = this.projects.filter((p) => p.ID !== ID);
+
+    if (newProjectsList) {
+      this.projects = newProjectsList;
+      this.saveDataToLocalStorage();
+      return of(true);
+    } else {
+      return of(false);
+    }
+  }
+}
